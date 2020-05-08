d01e382dc5d78fe703726a9bd077b19ddfdc9f3a
LeoYoung <yangtao@23mofang.com>
2020-05-08 16:38:43 +0800

    feat: setup angular

--- README.md
+++ README.md
@@ -1,24 +1,27 @@
-# [Leo Young Blog](https://leoyoung07.github.io/blog/)
+# LeoyoungBlog
 
-> A blog based on GitHub issues: [https://leoyoung07.github.io/blog](https://leoyoung07.github.io/blog)
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.3.
 
-## Build Setup
+## Development server
 
-``` bash
-# install dependencies
-npm install
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-# serve with hot reload at localhost:8080
-npm run dev
+## Code scaffolding
 
-# build for production with minification
-npm run build
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
 
-# lint all *.js and *.vue files
-npm run lint
+## Build
 
-# run unit tests
-npm test
-```
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
 
-For more information see the [docs for vueify](https://github.com/vuejs/vueify).
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- karma.conf.js
+++ karma.conf.js
@@ -1,23 +1,32 @@
-// https://github.com/Nikku/karma-browserify
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
 module.exports = function (config) {
   config.set({
-    browsers: ['ChromeHeadless'],
-    frameworks: ['browserify', 'mocha', 'chai'],
-    files: [
-      'node_modules/babel-polyfill/dist/polyfill.js',
-      'test/unit/**/*.js'
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
-    reporters: ['spec'],
-    preprocessors: {
-      'test/unit/**/*.js': ['browserify']
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    browserify: {
-      debug: true,
-      // needed to enable mocks
-      plugin: [require('proxyquireify').plugin]
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage/leoyoung-blog'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
     },
-    // if you want to continuously re-run tests on file-save,
-    // replace the following line with `autoWatch: true`
-    singleRun: true
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true
   });
 };
